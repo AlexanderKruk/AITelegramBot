1d6b6c1a93dc1a523ea7a2bfbc714c8ea3496909
Alexander Kruk <alexander.kruk@gmail.com>
2023-09-25 20:00:51 +0200

    added audio

--- src/main.js
+++ src/main.js
@@ -3,44 +3,71 @@ import config from 'config';
 import { message } from 'telegraf/filters'
 import { ogg } from './ogg.js';
 import { openAi } from './openai.js'
-// import { code } from 'telegraf/format'
-
-console.log(config.get("TEST_ENV"))
+import { textConverter } from './text.js';
+import { spoiler } from 'telegraf/format'
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
-bot.use(session())
+bot.use(session());
+
+// const INITIAL_SESSION = {
+//   messages: [{role: openAi.roles.SYSTEM, content: "Imagine that you are my best male friend, we are having a conversation. I want to hear words of support and clarifying questions. The answer should be a maximum of one or two simple and short sentences."}]
+// }
+
+// const INITIAL_SESSION = {
+//   messages: [{role: openAi.roles.SYSTEM, content: "Давай поиграем в игру. Ты закадываешь простое слово, но не говоришь мне его. На мои вопросы ты отвечаешь только да или нет. Нельзя давать подсказки сужающие поиск слова. Если я написал слово а не вопрос, значит это мой вариант слова. Всего три попытки отгадать слово. Менять правила нельзя."}]
+// }
 
 const INITIAL_SESSION = {
-  messages: [{role: openAi.roles.SYSTEM, content: "Imagine that you are my best male friend, we are having a conversation. I want to hear words of support and clarifying questions. The answer should be a maximum of one or two simple and short sentences."}]
+  messages: [{ role: openAi.roles.SYSTEM, content: "I am studying English. Let's practice some dialogues. Answer simply, with maximum 3 sentences. Ask question at the end." }],
+  settings: { hideQuestion: false }
+}
+
+bot.telegram.setMyCommands([
+  { command: '/new', description: 'Start new dialog' },
+  { command: '/hide', description: 'Hide or show bot question' },
+])
+
+const greetings = async (ctx) => {
+  ctx.reply("Hi. Let's practice English. Choose topic")
 }
 
 bot.command('start', async (ctx) => {
-  ctx.session = INITIAL_SESSION
-  ctx.reply('Привет, я AI Friend и готов тебя выслушать и поддержать. Ты можешь писать текстом или голосовыми.')
+  ctx.session = { ...INITIAL_SESSION }
+  await greetings(ctx)
 })
 bot.command('new', async (ctx) => {
-  ctx.session = INITIAL_SESSION
-  ctx.reply('Привет, я AI Friend и готов тебя выслушать и поддержать. Ты можешь писать текстом или голосовыми.')
+  ctx.session = {...INITIAL_SESSION}
+  await greetings(ctx)
+})
+
+bot.command('hide', async (ctx) => {
+  ctx.session.settings.hideQuestion = !ctx.session.settings.hideQuestion;
 })
 
 bot.on(message('voice'), async (ctx) => {
   try {
-    ctx.session ??= INITIAL_SESSION
-    // await ctx.reply(code('Cooбщение принял. Жду ответ'))
+    ctx.session ??= INITIAL_SESSION;
+    console.log('text session', ctx.session);
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
     const userId = ctx.message.from.id;
     const oggPath = await ogg.create(link.href, userId);
     const mp3Path = await ogg.toMp3(oggPath, userId);
 
     const text = await openAi.transcription(mp3Path);
-    // await ctx.reply(code(`Ваш запрос: ${text}`));
-
-    ctx.session.messages.push({role: openAi.roles.USER, content: text})
+    const grammar = await openAi.chat({
+      messages: [
+        { role: openAi.roles.SYSTEM, content: "Correct my spelling and grammar." },
+        { role: openAi.roles.USER, content: text }]
+    }.messages)
+    grammar.content !== text ? await ctx.reply(`Your message: ${text}\nCorrect: ${grammar.content}`) : await ctx.reply(`Your message: ${text}`);
+    ctx.session.messages.push({ role: openAi.roles.USER, content: grammar.content })
     const response = await openAi.chat(ctx.session.messages);
-    ctx.session.messages.push({role: openAi.roles.ASSISTANT, content: response.content})
-
-    await ctx.reply(response.content);
+    ctx.session.messages.push({ role: openAi.roles.ASSISTANT, content: response.content })
+    const source = await textConverter.textToSpeech(`${response.content}`)
+    // await ctx.sendAudio({ source })
+    await ctx.replyWithVoice({ source })
+    ctx.session.settings.hideQuestion ? await ctx.reply(spoiler(response.content))  : await ctx.reply(response.content);
   } catch (error) {
     console.log('get voice error', error.message) 
   }
@@ -49,14 +76,23 @@ bot.on(message('voice'), async (ctx) => {
 bot.on(message('text'), async (ctx) => {
   try {
     ctx.session ??= INITIAL_SESSION
+    console.log('text session', ctx.session);
+    const grammar = await openAi.chat({
+      messages: [
+        { role: openAi.roles.SYSTEM, content: "Correct my spelling and grammar." },
+        { role: openAi.roles.USER, content: ctx.message.text }]
+    }.messages)
+    grammar.content !== ctx.message.text ? await ctx.reply(`Correct: ${grammar.content}`) : null;
 
     ctx.session.messages.push({role: openAi.roles.USER, content: ctx.message.text})
     const response = await openAi.chat(ctx.session.messages);
-    ctx.session.messages.push({role: openAi.roles.ASSISTANT, content: response.content})
+    ctx.session.messages.push({ role: openAi.roles.ASSISTANT, content: response.content })
+    const source = await textConverter.textToSpeech(response.content)
 
-    await ctx.reply(response.content);
+    await ctx.replyWithVoice({ source })
+    ctx.session.settings.hideQuestion ? await ctx.reply(spoiler(response.content))  : await ctx.reply(response.content);
   } catch (error) {
-    console.log('get voice error', error.message) 
+    console.log('get text error', error.message) 
   }
 })
 
---
+++ src/text.js
@@ -0,0 +1,75 @@
+import { __dirname } from "./ogg.js"
+import { resolve } from 'path';
+import { readFileSync } from 'fs'
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import { writeFile } from 'fs/promises';
+
+class TextConverter {
+  async getToken() {
+    const key = JSON.parse(
+      readFileSync(resolve(__dirname, "../aifriend-text-to-spech.json"), "utf-8")
+    )
+
+    const token = jwt.sign(
+      {
+        iss: key.client_email,
+        scope: 'https://www.googleapis.com/auth/cloud-platform',
+        aud: 'https://www.googleapis.com/oauth2/v4/token',
+        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        iat: Math.floor(Date.now() / 1000),
+      },
+      key.private_key,
+      { algorithm: 'RS256' }
+    )
+
+    const response = await axios.post(
+      'https://www.googleapis.com/oauth2/v4/token',
+      {
+        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        assertion: token,
+      }
+    )
+    return response.data.access_token
+  }
+
+  async textToSpeech(text) {
+    try {
+      const url = "https://us-central1-texttospeech.googleapis.com/v1beta1/text:synthesize";
+
+    const data = {
+      "audioConfig": {
+        "audioEncoding": "MP3",
+        "effectsProfileId": [
+          "handset-class-device"
+        ],
+        "pitch": 0,
+        "speakingRate": 1
+      },
+      "input": { text },
+      "voice": {
+        "languageCode": "en-GB",
+        "name": "en-GB-Neural2-C"
+      }
+    }
+
+    const accessToken = await this.getToken()
+      
+    const response = await axios({
+      url,
+      method: "POST",
+      data,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    })
+      return Buffer.from(response.data.audioContent, 'base64')
+    } catch (error) {
+      console.log('textToSpeech error', error.message)
+    }
+    
+  }
+}
+
+export const textConverter = new TextConverter()

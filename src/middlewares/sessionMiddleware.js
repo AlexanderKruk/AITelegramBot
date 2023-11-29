187ee71cf988f57ee58ec0ecc2cd22ed6226f555
Alexander Kruk <alexander.kruk@gmail.com>
2023-11-29 10:04:50 +0100

    added redis password

--- src/middlewares/sessionMiddleware.js
+++ src/middlewares/sessionMiddleware.js
@@ -1,9 +1,11 @@
 import RedisSession from 'telegraf-session-redis-upd';
+import config from 'config';
 
 const session = new RedisSession({
   store: {
     host: process.env.TELEGRAM_SESSION_HOST || '127.0.0.1',
     port: process.env.TELEGRAM_SESSION_PORT || 6379,
+    password: config.get('REDIS_PASSWORD') || '',
   },
 });
 
